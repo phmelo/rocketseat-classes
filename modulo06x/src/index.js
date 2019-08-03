e0d245ec1ef7be1ec64dc5e479b301aef103d43c
Paulo <phmelomorais@gmail.com>
2019-08-03 00:44:20 -0300

    Configuring Reactotron

--- modulo06x/src/index.js
+++ modulo06x/src/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import './config/ReactotronConfig';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -15,11 +17,17 @@ const styles = StyleSheet.create({
   },
 });
 
+console.tron.log('Teste de debug no reactotron!');
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Hello!</Text>
-      <Text style={styles.welcome}>PH!</Text>
+      <Text style={styles.welcome}>ABCDEFG!</Text>
+      <Text style={styles.welcome}>HIJKLMNOP</Text>
+      <Text style={styles.welcome}>QRSTUVWXYZ</Text>
+      <Text style={styles.welcome}>Now I know my A B C</Text>
+      <Text style={styles.welcome}>Next time won't </Text>
+      <Text style={styles.welcome}>you sing with me?!</Text>
     </View>
   );
 }
