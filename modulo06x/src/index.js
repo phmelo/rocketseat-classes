3f15a1614eca00720b80443210aad1d8bf802fae
Paulo <phmelomorais@gmail.com>
2019-08-02 22:35:52 -0300

    Configuring routes

--- modulo06x/src/index.js
+++ modulo06x/src/index.js
@@ -1,33 +1,11 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 
 import './config/ReactotronConfig';
+import Routes from './routes';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-
-console.tron.log('Teste de debug no reactotron!');
+console.tron.log('Rotas Foi?!?');
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>ABCDEFG!</Text>
-      <Text style={styles.welcome}>HIJKLMNOP</Text>
-      <Text style={styles.welcome}>QRSTUVWXYZ</Text>
-      <Text style={styles.welcome}>Now I know my A B C</Text>
-      <Text style={styles.welcome}>Next time won't </Text>
-      <Text style={styles.welcome}>you sing with me?!</Text>
-    </View>
-  );
+  return <Routes />;
 }
