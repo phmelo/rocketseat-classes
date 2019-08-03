c4ce883358d9e2a6f0f958a4ebed9643f776a1ed
Paulo <phmelomorais@gmail.com>
2019-08-02 22:58:53 -0300

    primeiro commit

---
+++ modulo06x/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Hello!</Text>
+      <Text style={styles.welcome}>PH!</Text>
+    </View>
+  );
+}
