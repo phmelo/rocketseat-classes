ca3891492c856e91786057a5266236ed4dae43c1
Paulo <phmelomorais@gmail.com>
2019-08-11 12:50:52 -0300

    Configuring Home and Cart styles

--- modulo07/src/pages/Home/index.js
+++ modulo07/src/pages/Home/index.js
@@ -1,7 +1,95 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-cyber-feminino/34/D22-0699-234/D22-0699-234_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-cyber-feminino/34/D22-0699-234/D22-0699-234_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-cyber-feminino/34/D22-0699-234/D22-0699-234_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-cyber-feminino/34/D22-0699-234/D22-0699-234_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-cyber-feminino/34/D22-0699-234/D22-0699-234_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-cyber-feminino/34/D22-0699-234/D22-0699-234_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
