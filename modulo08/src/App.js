00d07b4e79048e16bd94917267902ad640e3f55d
Paulo <phmelomorais@gmail.com>
2019-08-12 20:23:53 -0300

    Hooks useEffect

--- modulo08/src/App.js
+++ modulo08/src/App.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [tech, setTech] = useState([]);
   const [newTech, setNewTech] = useState('');
 
   function handleAdd() {
     setTech([...tech, newTech]);
     setNewTech('');
   }
+
+  // Similar to ComponentDidMount
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+    // Similar to ComponentWillMount
+    // return () => {
+    //      document.removeEventListener(...);
+    // };
+  }, []);
+
+  // Similar to ComponentDidUpdate
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
 
   return (
     <>
